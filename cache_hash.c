83a165ba2dca4d8c6d4aa6db98a5cf35f82b7fa7
goalowrld <583032753@qq.com>
2013-02-06 18:01:19 +0800

    a

---
+++ cache_hash.c
@@ -0,0 +1,194 @@
+#define HASH_SET_S 1024
+struct _ListItem
+{
+	struct Entry ety;
+	struct _ListItem* next;
+	struct _ListItem* pre;
+};
+struct _List
+{
+	struct _ListItem * head,*tail;
+	unsigned minTimeout;
+};
+static void _listInit(struct _List *list);
+static void _listDestroy(struct _List *list);
+static struct _ListItem * _listInsert(struct _List *list,int key,struct UserData data,unsigned timeout);
+static void _listRemove(struct _List *list,struct _ListItem * litem);
+static int _listOnTimer(struct _List *list,unsigned times,remove_cb cb,void *cbarg);
+static void _listRealInsert(struct _List *list,struct _ListItem*item);
+struct HashList{
+	struct _List lists[HASH_SET_S];
+	unsigned minTimeout;
+	unsigned times;
+	int numItem;
+
+};
+#define HASH_FUNC(x)((x)%HASH_SET_S)
+void * 
+hashListNew()
+{
+	struct HashList * hl= malloc(sizeof(struct HashList));
+	int i;
+	for(;i<HASH_SET_S;i++){
+		_listInit(&hl->lists[i]);
+	}
+	return hl;
+}
+void 
+hashListDel(void *env)
+{
+	struct HashList * hl= (struct HashList *)env;
+	int i;
+	for(;i<HASH_SET_S;i++){
+		_listDestroy(&hl->lists[i]);
+	}
+}
+void *
+hashListInsert(void *arg,int key,struct UserData data,unsigned timeout)
+{
+	struct HashList * hl= (struct HashList *)arg;
+	unsigned t = timeout+hl->times;
+	int hash = HASH_FUNC(t);
+	void *ret = _listInsert(&hl->lists[hash],key,data,t);
+	if(ret){
+		if(t < hl->minTimeout){
+			hl->minTimeout = t;
+		}
+		if(hl->numItem == 0){
+		//startTimer();
+		}
+		hl->numItem ++;
+	}
+	return ret;
+}
+static void
+hashListRemove(void *arg,void *item)
+{
+	struct _ListItem *litem = (struct _ListItem*)item;
+	struct HashList * hl= (struct HashList *)arg;
+	int hash = HASH_FUNC(litem->ety.timeout);
+	_listRemove(&hl->lists[hash],litem);
+	hl->numItem--;
+	if(hl->numItem == 0){
+		hl->times = 0;
+		hl->minTimeout = -1;
+		//stopTimer();
+	}
+}
+static void
+hashListOnTimer(void *arg,unsigned times,remove_cb cb,void *cbarg)
+{
+	struct HashList * hl= (struct HashList *)arg;
+	if( hl->numItem > 0 ){
+		hl->times+=times;
+		printf("Times:%d  minTimeout:%d numItem:%d\n",hl->times,hl->minTimeout,hl->numItem);
+		if(hl->times >= hl->minTimeout){
+			int hash = HASH_FUNC(hl->times);
+			_listOnTimer(&hl->lists[hash],hl->times,cb,cbarg);
+		}
+	}
+}
+//---------------------------------------------------------------------------------
+static struct Entry 
+hashListGetEntryByItem( void *item)
+{
+	struct _ListItem * litem = (struct _ListItem *)(item);
+	return litem->ety;
+}
+static void 
+_listInit(struct _List *list)
+{
+	list->head = NULL;
+	list->tail = NULL;
+}
+static void 
+_listDestroy(struct _List *list)
+{
+	struct _ListItem *next,*cut = list->head;
+	while(cut){
+		next = cut->next;
+		free(cut);
+		cut = next;
+	}
+}
+
+
+static struct _ListItem *
+_listInsert(struct _List *list,int key,struct UserData data,unsigned timeout)
+{
+	struct _ListItem *p = malloc(sizeof(struct _ListItem));
+	p->ety.key = key;
+	p->ety.timeout = timeout;
+	p->ety.data = data;
+	p->next = NULL;
+	p->pre = NULL;
+	_listRealInsert(list,p);
+	return p;
+}
+static void
+_listRealInsert(struct _List *list,struct _ListItem*item)
+{
+	if(!list->head){
+		list->head = item;
+		list->tail = item;
+		return;
+	}
+	struct _ListItem *cut = list->head;
+	while(1){
+		//optimize 1 cpu idle case itemptr to array 2binary search timeout average chose tial or head
+		if(cut->ety.timeout >= item->ety.timeout){
+			if(!cut->pre){
+				list->head = item;
+			}else{
+				cut->pre->next = item;
+				item->pre = cut->pre;
+			}
+			item->next = cut;
+			cut->pre = item;
+			break;
+		}
+		if(!cut->next){
+			cut->next = item;
+			item->pre = cut;
+			list->tail = item;
+			break;
+		}
+		cut = cut->next;
+	}
+}
+static void
+_listRemove(struct _List *list,struct _ListItem * litem)
+{
+	if(litem->pre)litem->pre->next = litem->next;
+	if(litem->next)litem->next->pre = litem->pre;
+	if(litem == list->head){
+		list->head = litem->next;
+	}else if(litem == list->tail){
+		list->tail = litem->pre;
+	}
+	free(litem);
+}
+
+
+
+//
+static int
+_listOnTimer(struct _List *list,unsigned times,remove_cb cb,void *cbarg)
+{
+	int num = 0;
+	if( list->head){
+		while( list->head->ety.timeout <= times ){
+			struct _ListItem *tmp = list->head;
+			list->head = tmp->next;
+			cb(cbarg,tmp->ety);
+			free(tmp);
+			num++;
+			if( !list->head ){
+				break;
+			}
+		}
+		if(list->head)
+			list->minTimeout = list->head->ety.timeout;
+	}
+	return num;
+}
